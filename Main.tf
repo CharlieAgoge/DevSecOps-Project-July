e9d06107e36c7617deca8afa17aedab4e359bb5a
charlie <charlie.banyard@cyberagoge.comclear>
2024-09-15 15:46:33 +0100

    update

--- Main.tf
+++ Main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraformjulytest" {
-  bucket ="terraformjulytest"
+resource "aws_s3_bucket" "terraformjulytesting" {
+  bucket ="terraformjulytesting"
 }
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraformjulytest"
+    bucket         = "terraformjulytesting"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
   }
